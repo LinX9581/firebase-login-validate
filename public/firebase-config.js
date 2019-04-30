7e3978ccec91a1bc891ca3f782a28ed96a04bd73
linx9581 <linx9581@gmail.com>
2019-04-30 22:46:18 +0800

    add firebase-config.js

---
+++ public/firebase-config.js
@@ -0,0 +1,11 @@
+// firebase API
+var config = {
+    apiKey: "your firebase api",
+    authDomain: "artificial-intelligence-8da03.firebaseapp.com",
+    databaseURL: "url",
+    projectId: "artificial-intelligence-8da03",
+    storageBucket: "artificial-intelligence-8da03.appspot.com",
+    messagingSenderId: "id"
+};
+
+firebase.initializeApp(config);
