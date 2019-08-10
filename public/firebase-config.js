32c9c02820934e6f7b1217ec325654310ce4f2e7
linx9581 <linx9581@gmail.com>
2019-08-10 23:22:28 +0800

    change pai

--- public/firebase-config.js
+++ public/firebase-config.js
@@ -1,11 +1,11 @@
 // firebase API
 var config = {
-    apiKey: "your firebase api",
+    apiKey: "AIzaSyAN29JToq7jGGiol4xRHWPk0lAB_U_kGPY",
     authDomain: "artificial-intelligence-8da03.firebaseapp.com",
-    databaseURL: "url",
+    databaseURL: "https://artificial-intelligence-8da03.firebaseio.com",
     projectId: "artificial-intelligence-8da03",
     storageBucket: "artificial-intelligence-8da03.appspot.com",
-    messagingSenderId: "id"
+    messagingSenderId: "1095359509219"
 };
 
 firebase.initializeApp(config);
